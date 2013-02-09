7d437dba2619af7be52e5efb6e0570badad51ac9
Belyaev Gleb <Belyaev.Gleb@cg.ru>
2013-02-09 16:20:41 +0400

    example in-out

---
+++ src/example/ExampleBufferedReader.java
@@ -0,0 +1,13 @@
+package example;
+
+/**
+ * BufferedReader обеспечивает достаточно быстрый ввод для большинства задач.
+ * Но самостоятельно этот класс позволяет лишь читать отдельные символы и строки.
+ *
+ * @author Belyaev
+ */
+public class ExampleBufferedReader {
+  public static void main(String[] args) {
+//    BufferedReader in = new
+  }
+}
---
+++ src/example/ExampleStringTokenizer.java
@@ -0,0 +1,7 @@
+package example;
+
+/**
+ * @author Belyaev
+ */
+public class ExampleStringTokenizer {
+}
